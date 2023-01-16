286769fda6e3923482d61ed5145b0d20e38f7071
sgraziola <simo.fdl@gmail.com>
2023-01-16 18:51:38 +0100

    Add form requests and mass assignment for type

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,10 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
 
 class Type extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    public static function generateSlug($name)
+    {
+        $type_slug = Str::slug($name);
+        return $type_slug;
+    }
 
     /**
      * Get all of the projects for the Type
