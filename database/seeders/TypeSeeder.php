94cce9e10c84404452219db4f5d8d1b3de0dd550
sgraziola <simo.fdl@gmail.com>
2023-01-16 16:05:00 +0100

    create seeder and migration for foreignkey

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['E-Commerce', 'Mobile App', 'Game', 'Chatbot', 'Product Rating System', 'Online Shopping'];
+        foreach ($types as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name);
+            $newType->save();
+        }
+    }
+}
